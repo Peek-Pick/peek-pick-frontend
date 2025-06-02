74c2af35b5faadc333ced246133a3b7b5a6597fc
KimSoYeonnn <10sosokim10@gmail.com>
2025-06-02 13:19:36 +0900

    [🐛 fix] 깃 충돌 잡기
    - 깃 충돌로 바뀐 myPageComponent 수정

--- app/components/users/myPageComponent.tsx
+++ app/components/users/myPageComponent.tsx
@@ -5,43 +5,44 @@ import {
     FaQuestionCircle, FaBell, FaUserShield, FaFileContract, FaIdBadge, FaUserAltSlash
 } from 'react-icons/fa';
 import { IoLanguage, IoLogOutOutline } from "react-icons/io5";
+import { useNavigate } from "react-router";
 import { getMyPage } from "~/api/myPageAPI";
+import type { MyPageResponseDTO } from "~/types/users";
 
 // 타입 정의
 interface MypageData {
-    profileImgUrl: string;
+    profile_img_url: string;
     nickname: string;
     point: number;
-    wishlistedCount: number;
-    reviewCount: number;
-    couponCount: number;
-    barcodeHistoryCount: number;
+    wishlisted_count: number;
+    review_count: number;
+    coupon_count: number;
+    barcode_history_count: number;
 }
-
-
 
 export default function ProfileHeader() {
 
-    const initState:MypageData = {
-        profileImgUrl: '',
+    const initState = {
+        profile_img_url: '',
         nickname: '',
         point: 0,
-        wishlistedCount: 0,
-        reviewCount: 0,
-        couponCount: 0,
-        barcodeHistoryCount: 0
+        wishlisted_count: 0,
+        review_count: 0,
+        coupon_count: 0,
+        barcode_history_count: 0
     }
 
+    const navigate = useNavigate();
     const [myData, setMyData] = useState<MypageData>(initState);
 
     useEffect(() => {
         getMyPage()
             .then(result => {
                 const transformed = {
-                    profileImgUrl: result.profileImgUrl,
+                    profile_img_url: result.profile_img_url,
                     nickname: result.nickname,
                     point: result.point,
-                    ...result.quickStats, // quickStats 내부 값 펼쳐서 넣어야 함
+                    ...result.quick_stats, // quickStats 내부 값 펼쳐서 넣어야 함
                 };
                 setMyData(transformed);
             })
@@ -52,10 +53,10 @@ export default function ProfileHeader() {
 
     // 동적 quickStats
     const quickStats = [
-        { icon: <FaHeart className="text-pink-500 text-2xl mb-2" />, label: 'Wishlisted Items', value: myData.wishlistedCount },
-        { icon: <FaPen className="text-blue-500 text-2xl mb-2" />, label: 'My Reviews', value: myData.reviewCount },
-        { icon: <FaTicketAlt className="text-yellow-500 text-2xl mb-2" />, label: 'Coupons', value: myData.couponCount },
-        { icon: <FaBarcode className="text-green-500 text-2xl mb-2" />, label: 'Barcode History', value: myData.barcodeHistoryCount },
+        { icon: <FaHeart className="text-pink-500 text-2xl mb-2" />, label: 'Wishlisted Items', value: myData.wishlisted_count, to:'' },
+        { icon: <FaPen className="text-blue-500 text-2xl mb-2" />, label: 'My Reviews', value: myData.review_count, to:'' },
+        { icon: <FaTicketAlt className="text-yellow-500 text-2xl mb-2" />, label: 'Coupons', value: myData.coupon_count, to:'/mypoints/coupons' },
+        { icon: <FaBarcode className="text-green-500 text-2xl mb-2" />, label: 'Barcode History', value: myData.barcode_history_count, to:'' },
     ];
 
     const buttons: [string, React.ComponentType<React.SVGProps<SVGSVGElement>>][] = [
@@ -77,24 +78,30 @@ export default function ProfileHeader() {
                 <div className="text-center">
                     <div className="relative inline-block -mt-16">
                         <img
-                            src={myData.profileImgUrl || "/default.jpg"}
+                            src={`${myData.profile_img_url}`}
                             alt="Profile"
                             className="w-28 h-28 rounded-full border-4 border-white bg-white object-cover"
                         />
                     </div>
                     <h3 className="mt-4 mb-1 text-xl font-semibold">{myData.nickname}</h3>
                     <div className="flex justify-center mb-2">
-                        <button className="rounded flex items-center justify-center space-x-2 px-4 py-2 hover:bg-gray-100">
+                        <button
+                            onClick={ ()=> navigate('/mypoints/points/history')}
+                            className="rounded flex items-center justify-center space-x-2 px-4 py-2 hover:bg-gray-100">
                             <FaCoins className="text-indigo-500 text-xl" />
                             <p className="text-gray-500">{myData.point.toLocaleString()} Beeplet</p>
                             <FaAngleRight className="text-gray-500 text-xl" />
                         </button>
                     </div>
                     <div className="flex justify-center gap-2">
-                        <button className="border border-amber-300 text-amber-300 px-4 py-1 flex items-center hover:bg-gray-100">
+                        <button 
+                            onClick={ ()=> navigate('/points/store/list')}
+                            className="border border-amber-300 text-amber-300 px-4 py-1 flex items-center hover:bg-gray-100">
                             <FaStore className="mr-2" /> Point Store
                         </button>
-                        <button className="bg-amber-300 hover:bg-amber-400 active:bg-amber-200 text-white px-4 py-1 rounded flex items-center">
+                        <button
+                            onClick={ ()=> navigate('/mypage/edit')}
+                            className="bg-amber-300 hover:bg-amber-400 active:bg-amber-200 text-white px-4 py-1 rounded flex items-center">
                             <FaUserCog className="mr-2" /> Edit Profile
                         </button>
                     </div>
@@ -104,7 +111,9 @@ export default function ProfileHeader() {
             {/* 퀵 통계 */}
             <div className="grid grid-cols-2 md:grid-cols-4 gap-4 px-4 mb-6">
                 {quickStats.map((item, index) => (
-                    <button key={index} className="bg-white shadow hover:shadow-md transition rounded-xl p-4 flex flex-col items-center text-center w-full">
+                    <button key={index}
+                            onClick={ ()=> navigate(item.to)}
+                            className="bg-white shadow hover:shadow-md transition rounded-xl p-4 flex flex-col items-center text-center w-full">
                         {item.icon}
                         <span className="text-lg font-semibold text-gray-800">{item.value}</span>
                         <p className="text-sm font-medium text-gray-600 mb-1">{item.label}</p>
