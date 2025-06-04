a9e1fbeef3814f6b14cdf5dced1fb452d26a0bbc
bkm00 <bkm0096@gmail.com>
2025-06-04 13:21:18 +0900

    [🌱 feat] 페이지네이션 컴포넌트 추가

--- app/types/common.d.ts
+++ app/types/common.d.ts
@@ -2,3 +2,17 @@ export interface ActionResult<T> {
     result: string,
     data: T
 }
+
+export interface PagingResponse<T> {
+    content: T[];
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    number: number;  // 현재 페이지 (0-based)
+}
+
+export interface PaginationProps {
+    currentPage: number;  // 1-based
+    totalPages: number;
+    onPageChange: (page: number) => void;
+}
