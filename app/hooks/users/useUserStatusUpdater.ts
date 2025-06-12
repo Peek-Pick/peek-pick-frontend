eea751e19407e6533d1772da74cbcc63231d91f2
KimSoYeonnn <10sosokim10@gmail.com>
2025-06-12 13:34:09 +0900

    [📦 refactor] userStatus 변경 기능 수정
    - 정상적으로 성공했을 때만 화면을 갱신

--- app/hooks/users/useUserStatusUpdater.ts
+++ app/hooks/users/useUserStatusUpdater.ts
@@ -5,32 +5,31 @@ import {updateAdminUserStatus} from "~/api/users/adminUsersAPI";
 export function useUserStatusUpdater() {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState<string|null>(null)
-    const [success, setSuccess] = useState(false)
+    // const [success, setSuccess] = useState(false)
 
-    const updateStatus = async ({userId, updateStatus}: UpdateUserStatus) => {
+    const updateStatus = async ({userId, updateStatus}: UpdateUserStatus): Promise<boolean> =>{
         setLoading(true);
         setError(null);
-        setSuccess(false);
 
         try {
             await updateAdminUserStatus(
                 userId, {
-                status: updateStatus.status,
-                banUntil: updateStatus.status === "BANNED" ? updateStatus.banUntil : undefined
+                    status: updateStatus.status,
+                    banUntil: updateStatus.status === "BANNED" ? updateStatus.banUntil : undefined
                 })
 
-            setSuccess(true)
+                return true;
         } catch (err: any) {
             setError(err.response?.data?.message || "상태 변경 중 오류 발생")
         } finally {
             setLoading(false)
         }
     }
-
+    
     return {
         updateStatus,
         loading,
         error,
-        success
     }
 }
+
