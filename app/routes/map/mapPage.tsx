088bb6b694cd2d6aa03466c8d514e42ae46b6809
geunhwa37 <jacika37@naver.com>
2025-07-07 15:12:09 +0900

    [📦 refactor] 지도 언어설정 적용 - 지도 언어설정 적용 - 길찾기 에러 처리

--- app/routes/map/mapPage.tsx
+++ app/routes/map/mapPage.tsx
@@ -1,8 +1,33 @@
 
 import MapContainerComponent from "~/components/map/mapContainerComponent";
 import {BackButton} from "~/util/button/FloatingActionButtons";
+import MapLoadingComponent from "~/util/loading/mapLoadingComponent";
+import {type Libraries, useLoadScript} from "@react-google-maps/api";
+import i18n from "i18next";
+
+const libraries: Libraries = ['places'];
 
 const MapPage = () => {
+
+    // 구글 맵 API 스크립트 로드
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string,
+        language: i18n.language,
+        libraries,
+        version: 'weekly',
+    });
+
+
+    // 로드 에러 또는 로딩 중인 경우 처리
+    if (loadError) return "지도 로딩 오류";
+
+    if (!isLoaded) {
+        return (
+            <MapLoadingComponent/>
+        );
+    }
+
+
     return (
         <>
             <div>
